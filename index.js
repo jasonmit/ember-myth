d67499436a0026c47a23a38b4e342eb6ae8a46a8
Jason Mitchell <jason.mitchell.w@gmail.com>
2015-05-17 02:54:00 -0700

    fixing @import

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 var defaults     = require('lodash.defaults');
 var checker      = require('ember-cli-version-checker');
+var path         = require('path');
 
 var BroccoliMyth = require('./lib/broccoli-myth');
 
@@ -11,11 +12,7 @@ module.exports = EmberMyth;
 function MythPlugin (mythOptions) {
 	this.name = 'ember-myth';
 	this.ext  = 'css';
-
-	this.mythOptions = defaults({}, mythOptions, {
-		inputFile: 'app.css',
-		outputFile: 'app.css'
-	});
+	this.mythOptions = defaults({}, mythOptions);
 }
 
 MythPlugin.prototype = {
@@ -28,6 +25,7 @@ MythPlugin.prototype = {
 			trees.push(tree);
 		}
 
+		// todo: look into this `paths`
 		if (this.mythOptions.paths) {
 			trees = trees.concat(this.mythOptions.paths);
 		}
@@ -51,16 +49,29 @@ EmberMyth.prototype = {
 		return !checker.isAbove(this, '0.2.0');
 	},
 
-	setupPreprocessorRegistry: function(type, registry) {
-		var app    = this.app;
+	options: function(app) {
 		var config = this.project.config(app.env) || {};
+		var stylesDir;
 
 		var mythOptions = defaults({}, app.options.mythOptions, config.mythOptions, {
-			compress: app.env === 'production',
-			outputFile: this.project.name() + '.css'
+			paths: 'app/styles',
+			inputFile: 'app.css',
+			outputFile: 'app.css',
+			compress: app.env === 'production'
 		});
 
-		app.registry.add('css', new MythPlugin(mythOptions));
+		if (!mythOptions.source) {
+			stylesDir = app.options.trees.styles.dir || app.options.trees.styles
+			mythOptions.source = path.join(app.project.root, stylesDir, mythOptions.inputFile);
+		}
+
+		return mythOptions;
+	},
+
+	setupPreprocessorRegistry: function(type, registry) {
+		var app = registry.app;
+		var mythOptions = this.options(app);
+		registry.add('css', new MythPlugin(mythOptions));
 	},
 
 	included: function (app) {
